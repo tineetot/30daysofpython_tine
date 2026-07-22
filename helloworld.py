28ddb7495c78793ff1882cc69a1ad6421689d747
Kristine <detailzetsy@gmail.com>
2026-07-22 18:53:57 +0800

    finished exercise 1 - 3 of day 1 python challenge

---
+++ helloworld.py
@@ -0,0 +1,41 @@
+# Day 1 of my 30 Days of Python Challenge in Github!!
+# To check my python version, I will have to do the command in my terminal: python --version
+
+# EXERCISE LEVEL 1
+print (3 + 4) # getting the sum 
+print (3 - 4) # getting the difference 
+print (3 * 4) # getting the product 
+print (3 % 4) # getting the modulus 
+print (3 / 4) # getting the quotient 
+print (3 ** 4) # getting the exponential
+print (3 // 4) # getting the floor division operator 
+
+print ('Kristine') # my name
+print ('Borres') # my family name
+print ('Philippines') # my country name
+print ('I am absolutely LOVING the 30 days of Python') 
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Kristine'))
+print(type('Borres'))
+print(type('Philippines'))
+
+# EXERCISE LEVEL 2/3
+cutie = True
+
+print(type(25)) # integer
+print(type(2.15)) # float
+print(type(67j)) # complex
+print(type('tineetot')) # string
+print(type(cutie)) # boolean
+print(type((2, 15, 3, 4))) # tuple
+print(type([1, 2, 3, 4, 5])) # list
+print(type({'weh ba', 'really', 'jjinja'})) # set
+print(type({'cutie':'tinee'})) # dictionary
+
+# finding the euclidean distance between (2,3) and (10,8)
+print((3-2)^2 + (8-10)^2) # 3
